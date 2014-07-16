ad603876db55237a3183c88cccd2592d0c45b199
Andy Becker <acbecker@gmail.com>
2014-07-16 10:48:58 -0700

    Removing CCD N30 from the list; it appears to be missing

--- config/ingest.py
+++ config/ingest.py
@@ -11,11 +11,12 @@ root.parse.translators = {'date': 'translate_date',
                           'side': 'translate_side',
                           'ccd': 'translate_ccd',
                           }
+# Note that N30 may not be included, and if it is, is bad.  Remove from list for now.
 root.parse.extnames = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13',
                        'S14', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20', 'S21', 'S22', 'S23', 'S24', 'S25',
                        'S26', 'S27', 'S28', 'S29', 'S30', 'S31', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'N7',
                        'N8', 'N9', 'N10', 'N11', 'N12', 'N13', 'N14', 'N15', 'N16', 'N17', 'N18', 'N19',
-                       'N20', 'N21', 'N22', 'N23', 'N24', 'N25', 'N26', 'N27', 'N28', 'N29', 'N30', 'N31',
+                       'N20', 'N21', 'N22', 'N23', 'N24', 'N25', 'N26', 'N27', 'N28', 'N29', 'N31',
                        ]
 root.register.columns = {'visit':    'int',
                          'side':     'text',
