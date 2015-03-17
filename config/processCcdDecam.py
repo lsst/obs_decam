b5ac9c6c488d4ca4127def3faf325636750b07b7
Yusra AlSayyad <yusra@astro.washington.edu>
2015-03-17 16:18:51 -0700

    Increase default nCrPixelMax to 1% of pixels

--- config/processCcdDecam.py
+++ config/processCcdDecam.py
@@ -3,3 +3,4 @@ root.calibrate.background.undersampleStyle='REDUCE_INTERP_ORDER'
 root.detection.background.undersampleStyle='REDUCE_INTERP_ORDER'
 root.calibrate.initialPsf.fwhm=2.0
 root.calibrate.measurePsf.starSelector.name='catalog'
+root.calibrate.repair.cosmicray.nCrPixelMax=100000
