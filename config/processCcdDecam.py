0f10a39231da8c75a55bf7b864cd34e846c1e916
Yusra AlSayyad <yusra@astro.washington.edu>
2015-08-31 18:04:02 -0500

    Revert "Rename root to config in config override files"
    This reverts commit c5126538c6f89258fd9883bcfd506f3da9a66a96.

--- config/processCcdDecam.py
+++ config/processCcdDecam.py
@@ -1,6 +1,6 @@
-config.calibrate.detection.background.undersampleStyle='REDUCE_INTERP_ORDER'
-config.calibrate.background.undersampleStyle='REDUCE_INTERP_ORDER'
-config.detection.background.undersampleStyle='REDUCE_INTERP_ORDER'
-config.calibrate.initialPsf.fwhm=2.0
-config.calibrate.measurePsf.starSelector.name='catalog'
-config.calibrate.repair.cosmicray.nCrPixelMax=100000
+root.calibrate.detection.background.undersampleStyle='REDUCE_INTERP_ORDER'
+root.calibrate.background.undersampleStyle='REDUCE_INTERP_ORDER'
+root.detection.background.undersampleStyle='REDUCE_INTERP_ORDER'
+root.calibrate.initialPsf.fwhm=2.0
+root.calibrate.measurePsf.starSelector.name='catalog'
+root.calibrate.repair.cosmicray.nCrPixelMax=100000
