7f2b655866faddb55f448728687f7bf4044f8846
Hsin-Fang Chiang <hchiang2@illinois.edu>
2016-03-28 15:46:33 -0500

    Add config and data products for multiband processing

---
+++ config/multiBandDriver.py
@@ -0,0 +1,9 @@
+# Load from sub-configurations
+import os.path
+
+from lsst.utils import getPackageDir
+
+for sub in ("mergeCoaddDetections", "measureCoaddSources", "mergeCoaddMeasurements", "forcedPhotCoadd"):
+    path = os.path.join(getPackageDir("obs_decam"), "config", sub + ".py")
+    if os.path.exists(path):
+        getattr(config, sub).load(path)
