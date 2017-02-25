24b84177c04b9939e80dd1a1655b06aa4078707a
Meredith Rawls <mrawls@uw.edu>
2017-02-24 16:31:18 -0800

    Remove "path" as a required field from the calib registry

--- config/ingestCalibs.py
+++ config/ingestCalibs.py
@@ -1,6 +1,8 @@
 from lsst.obs.decam.ingestCalibs import DecamCalibsParseTask
 config.parse.retarget(DecamCalibsParseTask)
 config.parse.hdu = 1
+config.parse.translation = {"obstype": "OBSTYPE"}
+
 # N30 is not included becasue it is not functional.
 config.parse.extnames = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13',
                          'S14', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20', 'S21', 'S22', 'S23', 'S24', 'S25',
@@ -14,7 +16,6 @@ config.parse.translators = {'filter': 'translate_filter',
                             }
 config.register.columns = {'filter': 'text',
                            'ccdnum': 'int',
-                           'path': 'text',
                            'calibDate': 'text',
                            'validStart': 'text',
                            'validEnd': 'text',
