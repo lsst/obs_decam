845a89c0ca56a43525c89648fb783c16a11ab068
Jim Bosch <jbosch@astro.princeton.edu>
2019-09-18 14:38:29 -0500

    Update for upstream changes in daf_butler, obs_base, and pipe_tasks.

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -1,3 +1,4 @@
 config.match.refObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
 config.match.refObjLoader.filterMap['u'] = 'g'
 config.match.refObjLoader.filterMap['Y'] = 'y'
+config.connections.refCat = "ps1_pv3_3pi_20170110"
