f6920f150290578cfcb0cb0a2e1ba4506cbd4d14
Krzysztof Findeisen <kfindeis@uw.edu>
2020-02-26 15:22:52 -0600

    Remove deblending footprint size override.
    The override was proactively added on DM-11521 for
    backward-compatibility, but was never actually used (because all
    DECam processing used the CP config). We don't want historical
    overrides that aren't actually justified by DECam-specific needs.

--- config/calibrate.py
+++ config/calibrate.py
@@ -16,8 +16,5 @@ config.connections.astromRefCat = "ps1_pv3_3pi_20170110"
 config.connections.photoRefCat = "ps1_pv3_3pi_20170110"
 config.photoCal.photoCatName = "ps1_pv3_3pi_20170110"
 
-# this was the default prior to DM-11521.  New default is 2000.
-config.deblend.maxFootprintSize = 0
-
 # Set SIP order to 4, the Task default before RFC-577 to maintain same behavior
 config.astrometry.wcsFitter.order = 4
