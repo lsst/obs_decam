7ff139e44ed00fb4d47a7407bf5b131a951abc77
Krzysztof Findeisen <kfindeis@uw.edu>
2020-02-26 15:45:21 -0600

    Deprecate processCcdCpIsr.py.
    The config was once needed to correctly process data with CP
    calibrations, but the standard IsrTask now handles this case
    correctly. Having two config files for ProcessCcd has led to
    some bit rot and is best avoided.

--- config/processCcdCpIsr.py
+++ config/processCcdCpIsr.py
@@ -2,9 +2,13 @@
 DECam-specific overrides for ProcessCcdTask/Community Pipeline products
 """
 import os.path
+import warnings
 
 obsConfigDir = os.path.dirname(__file__)
 
-config.isr.load(os.path.join(obsConfigDir, 'isr.py'))
-config.charImage.load(os.path.join(obsConfigDir, 'characterizeImage.py'))
-config.calibrate.load(os.path.join(obsConfigDir, 'calibrate.py'))
+config.load(os.path.join(obsConfigDir, 'processCcd.py'))
+
+warnings.warn(
+    'processCcdCpIsr.py is no longer needed, use processCcd.py instead. Will '
+    'be removed after release 21.0',
+    category=FutureWarning)
