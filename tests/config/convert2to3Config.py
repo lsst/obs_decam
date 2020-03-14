db183a75a7ced86f709406761adcb224225c8701
John Parejko <parejkoj@uw.edu>
2020-03-13 17:00:35 -0700

    Use new testdata gen2 repo for convert and formatter tests

--- tests/config/convert2to3Config.py
+++ tests/config/convert2to3Config.py
@@ -1,4 +1,5 @@
 # Config overrides for `test_convert2to3.py`
-# The decam conversion tests use `ap_verify_ci_hits2015`, which has two refcats.
+# The decam conversion tests use stripped data from `ap_verify_ci_hits2015` in
+# testdata_decam, with two refcats in the repo.
 
 config.refCats = ['gaia', 'panstarrs']
