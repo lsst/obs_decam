5244014285078519210dd8ba91623e6f636b2d9f
John Swinbank <swinbank@lsst.org>
2020-04-09 17:31:16 -0700

    Clarify plans for wcsFitter.order.

--- config/calibrate.py
+++ config/calibrate.py
@@ -16,5 +16,9 @@ config.connections.astromRefCat = "ps1_pv3_3pi_20170110"
 config.connections.photoRefCat = "ps1_pv3_3pi_20170110"
 config.photoCal.photoCatName = "ps1_pv3_3pi_20170110"
 
-# Set SIP order to 4, the Task default before RFC-577 to maintain same behavior
+# The Task default was reduced from 4 to 2 on RFC-577. We believe that 4 is
+# more appropriate for use with DECam data until a Jointcal-derived distortion
+# model is available (DM-24431); at that point, this override should likely be
+# removed.
+# See Slack: https://lsstc.slack.com/archives/C2B6X08LS/p1586468459084600
 config.astrometry.wcsFitter.order = 4
