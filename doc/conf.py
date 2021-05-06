886f863b3600f9d7cdc56587d50bee7026834a41
Jonathan Sick <jsick@lsst.org>
2021-05-05 21:13:22 -0400

    Sync with template

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,4 @@
 """Sphinx configuration file for an LSST stack package.
-
 This configuration only affects single-package Sphinx documentation builds.
 For more information, see:
 https://developer.lsst.io/stack/building-single-package-docs.html
