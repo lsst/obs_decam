c57d4568e43aac799b44cd92e3725fe3d721cc94
John Parejko <parejkoj@uw.edu>
2021-12-15 17:30:31 -0800

    Move yaml specifications for column transformation
    These were moved pipe_tasks/schemas/

--- config/transformSourceTable.py
+++
@@ -1,6 +0,0 @@
-import os.path
-
-# Use the environment var. to prevent hardcoding of paths into quantum graphs.
-# TODO: DM-28862: When that ticket is merged, policy files will be moved to a
-# central location, likely into pipe_tasks.
-config.functorFile = os.path.join('$OBS_DECAM_DIR', 'policy', 'Source.yaml')
