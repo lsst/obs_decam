de30c6b5754a9d1f2bd5a7d795a7227ca17b0f43
Krzysztof Findeisen <kfindeis@uw.edu>
2022-05-19 16:05:54 -0500

    Add instrument config for SkyBot query.

---
+++ config/SkyBotEphemerisQuery.py
@@ -0,0 +1,4 @@
+# DECam-specific config for lsst.ap.association.skyBotEphemerisQuery.SkyBotEphemerisQueryTask
+
+config.observerCode = "W84"        # IAU code for DECam
+config.queryRadiusDegrees = 1.3    # DECam FoV radius + 20% margin
