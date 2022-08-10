87b9da8adc689c9474954939f4fb19a582fa8167
Tim Jenness <tjenness@lsst.org>
2022-08-10 15:09:12 -0700

    Remove the gen2 runIsr configs

--- config/runIsrCp.py
+++
@@ -1,8 +0,0 @@
-"""
-DECam-specific overrides for RunIsrTask/Community Pipeline products
-"""
-import os.path
-
-obsConfigDir = os.path.join(os.path.dirname(__file__))
-
-config.isr.load(os.path.join(obsConfigDir, "isr.py"))
