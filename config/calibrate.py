d2b04f18cf25ba8391d5ded488d790c7e0c8c7ed
David Wang <ykwang@uw.edu>
2024-11-14 14:20:16 -0800

    zero point refcat culling

--- config/calibrate.py
+++ config/calibrate.py
@@ -1,25 +1,53 @@
 """
 DECam-specific overrides for CalibrateTask
 """
+
+# Testing DM-30994 with crowded Bulge Saha data
+
 import os.path
 
 from lsst.meas.algorithms import ColorLimit
 
-obsConfigDir = os.path.join(os.path.dirname(__file__))
+config_dir = os.path.dirname(__file__)
 
-config.photoRefObjLoader.load(os.path.join(obsConfigDir, "filterMap.py"))
+# Overrides to improved astrometry matching.
+config.astrometry.doFiducialZeroPointCull = True
+config.astrometry.load(os.path.join(config_dir, "fiducialZeroPoint.py"))
 
-# Photometric calibration: use color terms
-config.photoCal.applyColorTerms = True
-colors = config.photoCal.match.referenceSelection.colorLimits
-# The following two color limits are adopted from obs_subaru for the HSC
-# SSP survey.
+# Use PS1 for both astrometry and photometry.
+config.connections.astrometry_ref_cat = "ps1_pv3_3pi_20170110"
+config.astrometry_ref_loader.load(os.path.join(config_dir, "filterMap.py"))
+# Use the filterMap instead of the "any" filter (as is used for Gaia).
+config.astrometry_ref_loader.anyFilterMapsToThis = None
+config.photometry_ref_loader.load(os.path.join(config_dir, "filterMap.py"))
+
+# Use colorterms for photometric calibration, with color limits on the refcat.
+config.photometry.applyColorTerms = True
+config.photometry.photoCatName = "ps1_pv3_3pi_20170110"
+colors = config.photometry.match.referenceSelection.colorLimits
 colors["g-r"] = ColorLimit(primary="g_flux", secondary="r_flux", minimum=0.0)
 colors["r-i"] = ColorLimit(primary="r_flux", secondary="i_flux", maximum=0.5)
-config.photoCal.match.referenceSelection.doMagLimit = True
-config.photoCal.match.referenceSelection.magLimit.fluxField = "i_flux"
-config.photoCal.match.referenceSelection.magLimit.maximum = 22.0
-config.photoCal.colorterms.load(os.path.join(obsConfigDir, "colorterms.py"))
+config.photometry.colorterms.load(os.path.join(config_dir, "colorterms.py"))
+
+# import os.path
+
+# from lsst.meas.algorithms import ColorLimit
+
+# obsConfigDir = os.path.join(os.path.dirname(__file__))
+
+# config.photoRefObjLoader.load(os.path.join(obsConfigDir, "filterMap.py"))
+
+# # Photometric calibration: use color terms
+# config.photoCal.applyColorTerms = True
+# colors = config.photoCal.match.referenceSelection.colorLimits
+# # The following two color limits are adopted from obs_subaru for the HSC
+# # SSP survey.
+# colors["g-r"] = ColorLimit(primary="g_flux", secondary="r_flux", minimum=0.0)
+# colors["r-i"] = ColorLimit(primary="r_flux", secondary="i_flux", maximum=0.5)
+# config.photoCal.match.referenceSelection.doMagLimit = True
+# config.photoCal.match.referenceSelection.magLimit.fluxField = "i_flux"
+# config.photoCal.match.referenceSelection.magLimit.maximum = 22.0
+# config.photoCal.colorterms.load(os.path.join(obsConfigDir, "colorterms.py"))
 
 # The Task default was reduced from 4 to 2 on RFC-577. We believe that 4 is
 # more appropriate for use with DECam data until a Jointcal-derived distortion
---
+++ config/fiducialZeroPoint.py
@@ -0,0 +1,11 @@
+# DM-30993 
+
+# Fiducial ZeroPoint for 1s exposure
+config.fiducialZeroPoint = {
+    "u": 21.63,
+    "g": 25.11,
+    "r": 25.35,
+    "i": 25.2,
+    "z": 25.0,
+    "y": 24.2,
+}
