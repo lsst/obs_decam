3273f5fc3a3cc91c810b73b37ea97d05f32ad6d0
Jim Bosch <jbosch@astro.princeton.edu>
2024-11-26 20:25:39 -0500

    Remove configs and tests for deprecated object-refcat match outputs.

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -5,12 +5,4 @@ config.measurement.load(os.path.join(os.path.dirname(__file__), "kron.py"))
 config.measurement.load(os.path.join(os.path.dirname(__file__), "convolvedFluxes.py"))
 config.measurement.load(os.path.join(os.path.dirname(__file__), "hsm.py"))
 config.load(os.path.join(os.path.dirname(__file__), "cmodel.py"))
-
-# Set reference catalog for Gen3.
-config.connections.refCat = "ps1_pv3_3pi_20170110"
-
-config.match.refObjLoader.load(os.path.join(os.path.dirname(__file__), "filterMap.py"))
-
-config.doWriteMatchesDenormalized = True
-
 config.measurement.plugins.names |= ["base_InputCount"]
