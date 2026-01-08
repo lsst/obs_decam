5975d93593914105d0b3239929ad6cb09ce67b95
Jim Bosch <jbosch@astro.princeton.edu>
2026-01-08 09:51:02 -0500

    Disable writing visit geometry when pointing refit is disabled.
    The default is already False for UpdateVisitSummaryTask, but since
    task validation will fail when do_write_visit_geometry=True and
    do_refit_pointing=False, it makes sense to set it again here.

--- config/updateVisitSummary.py
+++ config/updateVisitSummary.py
@@ -1,3 +1,4 @@
 # The refit-pointing subtask does not yet support instruments with a parity
 # flip between FOCAL_PLANE and FIELD_ANGLE.
 config.do_refit_pointing = False
+config.do_write_visit_geometry = False
