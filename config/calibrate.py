fa660635719e084dd9b1d9c05ec8fb04e5bb5fec
Matthias Wittgen <Matthias.Michael.Wittgen@CERN.CH>
2026-08-31 15:06:53 -0700

    Replace getPackageDir by ResourcePath

--- config/calibrate.py
+++ config/calibrate.py
@@ -1,13 +1,9 @@
 """
 DECam-specific overrides for CalibrateTask
 """
-import os.path
-
 from lsst.meas.algorithms import ColorLimit
 
-obsConfigDir = os.path.join(os.path.dirname(__file__))
-
-config.photoRefObjLoader.load(os.path.join(obsConfigDir, "filterMap.py"))
+config.photoRefObjLoader.load("filterMap.py")
 
 # Photometric calibration: use color terms
 config.photoCal.applyColorTerms = True
@@ -19,7 +15,7 @@ colors["r-i"] = ColorLimit(primary="r_flux", secondary="i_flux", maximum=0.5)
 config.photoCal.match.referenceSelection.doMagLimit = True
 config.photoCal.match.referenceSelection.magLimit.fluxField = "i_flux"
 config.photoCal.match.referenceSelection.magLimit.maximum = 22.0
-config.photoCal.colorterms.load(os.path.join(obsConfigDir, "colorterms.py"))
+config.photoCal.colorterms.load("colorterms.py")
 
 # The Task default was reduced from 4 to 2 on RFC-577. We believe that 4 is
 # more appropriate for use with DECam data until a Jointcal-derived distortion
@@ -28,9 +24,9 @@ config.photoCal.colorterms.load(os.path.join(obsConfigDir, "colorterms.py"))
 # See Slack: https://lsstc.slack.com/archives/C2B6X08LS/p1586468459084600
 config.astrometry.wcsFitter.order = 4
 
-config.measurement.load(os.path.join(obsConfigDir, "apertures.py"))
-config.measurement.load(os.path.join(obsConfigDir, "kron.py"))
-config.measurement.load(os.path.join(obsConfigDir, "hsm.py"))
+config.measurement.load("apertures.py")
+config.measurement.load("kron.py")
+config.measurement.load("hsm.py")
 
 config.measurement.plugins.names |= ["base_Jacobian", "base_FPPosition"]
 config.measurement.plugins["base_Jacobian"].pixelScale = 0.263
